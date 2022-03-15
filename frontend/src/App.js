156612457e9e20608d76f4e7269a5de02359098b
Emmanuel Tweneboah Koduah <emmanueltweneboah71@gmail.com>
2022-03-15 12:11:19 +0000

    frontend/ added a navbar , footer and a landing page components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import LandingPage from "./Screens/LandingPage/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+    <>
+      <Header />
+      <main>
+        <LandingPage />
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="primary" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Tkayy Note Taker</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="m-auto">
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+            </Form>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#home">My Notes</Nav.Link>
+            <NavDropdown title="Emmanuel" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.2">My Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
