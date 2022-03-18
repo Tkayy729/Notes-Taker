58a592d5eeffdbb3a292267544debbf3e6df295f
Emmanuel Tweneboah Koduah <emmanueltweneboah71@gmail.com>
2022-03-18 15:32:27 +0000

    implemened the user  register and login handlers

---
+++ backend/controller/userController.js
@@ -0,0 +1,53 @@
+const expressAsyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = expressAsyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error Occured!!");
+  }
+});
+
+const authUser = expressAsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+module.exports = { registerUser, authUser };
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ const dotenv = require("dotenv");
 const notes = require("./data/notes");
 var cors = require("cors");
 const ConnectDB = require("./config/db");
+const UserRoute = require("./routes/userRoute");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
+app.use(express.json());
 dotenv.config();
 ConnectDB();
 
@@ -19,6 +22,10 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.send("API is running ...");
 });
+
+app.use("/api/users", UserRoute);
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/api/notes", (req, res) => {
   res.json(notes);
